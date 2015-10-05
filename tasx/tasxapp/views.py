d17d27e87afee68e427f0401da3a9dedeb707054
lcsexr <lucas.evans.carpi@gmail.com>
2015-10-05 04:19:19 -0400

    adding some html

--- tasx/tasxapp/views.py
+++ tasx/tasxapp/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 from .models import Report
 # Create your views here.
 
 def index(request):
-    return render(request,'home/home.html', {})
-
-
+    try:
+        reports = Report.objects.all()
+        ##reports = Report.objects.filter(date=timezone.now()).order_by('date')
+    except Report.DoesNotExist:
+        raise Http404("Theres no reports.")
+    return render(request,'home/home.html', {'reports' : reports})
 
 
 #def index(request):
