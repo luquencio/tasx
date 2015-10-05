dbf05c5b29701cc3c6c804680d6acbae3eb8cbdc
lcsexr <lucas.evans.carpi@gmail.com>
2015-10-05 03:10:33 -0400

    views fixed

--- tasx/tasxapp/views.py
+++ tasx/tasxapp/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-
+from .models import Report
 # Create your views here.
 
 def index(request):
-    return render(render,'home/home.html', {})
+    return render(request,'home/home.html', {})
 
 
 
